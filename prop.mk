afd70385d4d446928de96a04ec428c75cf456243
Daniel Sikorski <dadi.sikorski@gmail.com>
2020-07-09 11:47:33 +0530

    vince: dex2oat: use 8 cores instead of 4
    use more cores for faster app installation
    
    Change-Id: I7045673a28eed043ec45013b1abc48e5f3a5f7ba
    
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -142,6 +142,7 @@ persist.sys.usb.config=diag,serial_smd,rmnet_ipa,adb
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so
 
 # Netmgrd
